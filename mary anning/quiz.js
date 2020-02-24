f384d9075c240370e3c092f7c17ea1a03c531774
modernman00 <waleolaogunrac@gmail.com>
2020-02-24 17:03:17 +0000

    move to php

--- mary anning/quiz.js
+++ mary anning/quiz.js
@@ -1,44 +1,32 @@
-const ageAnswer = 50; // PASS MARK
-const age1 = document.getElementById("inlineRadio1"); // FIRST OPTION
-const age2 = document.getElementById("inlineRadio2"); // SECOND OPTION
+const get_id = x => document.getElementById(x);
+const log = x => console.log(x);
 
-// get all the childnodes
-const allDiv = document.getElementById("quizSection");
-const nodeListDiv = allDiv.childNodes;
+const ageAnswer = 32; // PASS MARK
+var feedback = get_id("resultShow");
+const submit = get_id("button");
+const ans = ["You have chosen the right answer", "Incorrect - Please try again"];
+// function to change the answer's background color:
+const bgChange = () => {
+  feedback.style.backgroundColor =
+    (feedback.innerHTML == ans[0]) ? "green" : "red";
+};
 
-var feedback = document.getElementById("resultShow");
-
-// pick the values of the radio
-const age1Radio = nodeListDiv[5].firstElementChild.value;
-const age2Radio = nodeListDiv[7].firstElementChild.value;
-const correct = "You have chosen the right answer";
-const incorrect = "Incorrect - Please try again";
-
-// function to change the answer's background color: 
-
-const bgChange =()=> {
+const checkAnswer2 = () => {
+  const theForm = get_id("quizForm");
+  const radioName = theForm.elements.quiz;
+  // log(radioName);
+  // let olu = [1, 2, 4, 5, 6];
+  for (let i = 0; i < radioName.length; i++) {
+    if (radioName[i].checked) {
     
-    feedback.style.backgroundColor = (feedback.innerHTML == correct) ? "green" : "red";
-};
+      feedback.innerHTML = (parseInt(radioName[i].value) === ageAnswer) ? ans[0] : ans[1];
+    } 
+  
+  }
 
-// function to activate once clicked
-
-const check1 = ()=> {
-
-    feedback.innerHTML = (age1Radio == ageAnswer)? correct : incorrect;
-    bgChange();
-};
-
-
-const check2 = () => {
-
-  feedback.innerHTML = age2Radio == ageAnswer ? correct : incorrect;
+  // function for change of bg color
   bgChange();
 };
 
+submit.addEventListener("click", checkAnswer2);
 
-// check once click if the value is the right answer
-
-age1.addEventListener("click", check1);
-
-age2.addEventListener("click", check2);
